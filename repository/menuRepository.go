d4204b6883cc87508e2a901a8a961688c3fbc159
AryaTabani <a.tabani82@gmail.com>
2025-07-21 12:18:13 +0330

    implementing menuService

--- repository/menuRepository.go
+++ repository/menuRepository.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 
 	db "example.com/m/v2/DB"
+	"example.com/m/v2/models"
 )
 
 var (
@@ -13,7 +14,7 @@ var (
 
 func MenuExists(menuID string) (bool, error) {
 	var id int64
-	err := db.DB.QueryRow(`SELECT id FROM menus WHERE menu_id = ?`, menuID).Scan(&id)
+	err := db.DB.QueryRow(`SELECT id FROM menus WHERE menuid = ?`, menuID).Scan(&id)
 	if err != nil {
 		if err == sql.ErrNoRows {
 			return false, nil
@@ -21,4 +22,29 @@ func MenuExists(menuID string) (bool, error) {
 		return false, err
 	}
 	return true, nil
+}
+func CreateMenu(menu *models.Menu) (int64, error) {
+	stmt, err := db.DB.Prepare(`
+		INSERT INTO menus (name, category, startdate, enddate, createdat, updatedat, menuid)
+		VALUES (?, ?, ?, ?, ?, ?, ?)
+	`)
+	if err != nil {
+		return 0, err
+	}
+	defer stmt.Close()
+
+	res, err := stmt.Exec(
+		menu.Name,
+		menu.Category,
+		menu.Start_Date,
+		menu.End_Date,
+		menu.Created_at,
+		menu.Updated_at,
+		menu.Menu_id,
+	)
+	if err != nil {
+		return 0, err
+	}
+
+	return res.LastInsertId()
 }
